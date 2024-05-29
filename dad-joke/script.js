b1644261fbb44c55bb184b3a422e4230d170938f
Anna Melnyk <blairw73@gmail.com>
2024-05-29 22:40:42 +1000

    Add headers

--- dad-joke/script.js
+++ dad-joke/script.js
@@ -3,17 +3,20 @@
 const button = document.querySelector(".button")
 const joke = document.querySelector(".joke-text")
 
-window.addEventListener('load', getJoke)
+window.addEventListener("load", getJoke)
 button.addEventListener("click", getJoke)
 
 async function getJoke() {
   try {
-    const resp = await fetch("https://icanhazdadjoke.com/slack")
+    const resp = await fetch("https://icanhazdadjoke.com/slack", {
+      headers: {
+        Accept: "application/json"
+      }
+    })
     const jokeResp = await resp.json()
 
     joke.textContent = jokeResp.attachments[0].text
-  } catch(err) {
+  } catch (err) {
     console.error(err.message)
   }
 }
-
