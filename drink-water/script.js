d11f8707275536108cd145851a41940e5dbe80d6
Anna Melnyk <blairw73@gmail.com>
2024-06-04 23:32:05 +1000

    Add project part2

--- drink-water/script.js
+++ drink-water/script.js
@@ -1,25 +1,61 @@
 "use strict"
 
+const CLASS_FILLED = "filled"
 const smallGlass = document.querySelectorAll(".glasses .glass")
 const glassesContainer = document.querySelector(".glasses")
 const remainingAmountNode = document.querySelector(".remained-amount")
+const remainedNode = document.querySelector(".remained")
 let filledAmountNode = document.querySelector(".filled-amount")
+const MAX_VOLUME = 2000
 
 glassesContainer.onclick = function (e) {
   if (e.target.className === "glass") {
-    e.target.classList.add("filled")
+    e.target.classList.add(CLASS_FILLED)
+
+    const lastIndex = Array.from(smallGlass).findLastIndex((g) =>
+      g.className.includes(CLASS_FILLED)
+    )
+    for (let i = 0; i <= lastIndex; i++) {
+      if (!smallGlass[i].className.includes(CLASS_FILLED)) {
+        fillBigGlass()
+        smallGlass[i].classList.add(CLASS_FILLED)
+      }
+    }
     fillBigGlass()
   }
 }
 
 function fillBigGlass() {
-  let remainingAmountInMl = remainingAmountNode.textContent.split("L")[0] * 1000
-  console.log("remainingAmountInMl", remainingAmountInMl)
+  const filledAmount = calculateFilledAmountInMl()
+  const remainingAmount = calculateRemainingAmountInMl(filledAmount)
+  const filledAmountNodeContent = `${(filledAmount * 100) / MAX_VOLUME}%`
+
+  remainingAmountNode.textContent = `${remainingAmount / 1000}L`
+  filledAmountNode.textContent = filledAmountNodeContent
+  filledAmountNode.style.height = filledAmountNodeContent
+
+  if (filledAmountNodeContent === "100%") hideRemainingInfo()
+}
+
+function calculateRemainingAmountInMl(filledAmount) {
+  return MAX_VOLUME - filledAmount
+}
+
+function calculateFilledAmountInMl() {
   let filledAmount = filledAmountNode.textContent.split("%")[0]
-  console.log("filledAmount", filledAmount)
-  if (!filledAmount) filledAmount = 0
-  filledAmount = filledAmount * 100 + 250
-  remainingAmountInMl = remainingAmountInMl - filledAmount
-  remainingAmountNode.textContent = `${remainingAmountInMl / 1000}L`
-  filledAmountNode.textContent = `${filledAmount / 100}%`
+
+  if (!filledAmount) {
+    filledAmount = 0
+  } else {
+    filledAmount = (Number(filledAmount) * MAX_VOLUME) / 100
+  }
+
+  filledAmount = Number(filledAmount) + 250
+
+  return filledAmount
+}
+
+function hideRemainingInfo() {
+  remainingAmountNode.style.display = "none"
+  remainedNode.style.display = "none"
 }
