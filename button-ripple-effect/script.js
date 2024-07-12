d3b1288e3c9c7d9b667bdfc1bb8c0d8bb90f56fe
Anna Melnyk <blairw73@gmail.com>
2024-07-12 23:41:14 +1000

    Add project

--- button-ripple-effect/script.js
+++ button-ripple-effect/script.js
@@ -1,1 +1,14 @@
 "use strict"
+
+const button = document.querySelector(".btn")
+
+button.addEventListener("click", (e) => {
+  const ripple = document.querySelector(".ripple")
+  const newNode = ripple.cloneNode(true)
+
+  newNode.classList.add("animate")
+  newNode.style.left = e.offsetX + "px"
+  newNode.style.top = e.offsetY + "px"
+
+  button.replaceChild(newNode, ripple)
+})
