3442056cbf6b9cefeff7f335d2ef14b999874a22
Anna Melnyk <blairw73@gmail.com>
2024-07-18 12:23:46 +1000

    Add changes

--- drag-n-drop/script.js
+++ drag-n-drop/script.js
@@ -1,22 +1,53 @@
 "use strict"
 
 const main = document.querySelector(".main")
+let emptyDivs
 
 createDraggableBlocks()
 
 function createDraggableBlocks() {
   for (let i = 0; i < 5; i++) {
-    const block = document.createElement("div")
-    block.classList.add('block')
-    block.setAttribute('draggable', true)
-    main.appendChild(block)
-    addDragListeners(block)
+    const empty = document.createElement("div")
+    empty.classList.add('empty')
+    main.appendChild(empty)
+    addDragListeners(empty)
   }
+
+  emptyDivs = document.querySelectorAll('.empty')
+  const block = document.createElement("div")
+  block.classList.add('block')
+  block.setAttribute('draggable', true)
+  emptyDivs[0].appendChild(block)
 }
+
 
 function addDragListeners(el) {
   el.addEventListener("dragstart", event => {
     console.log('dragstart')
-    event.dataTransfer.setData("text/plain", "This text may be dragged")
+    console.log(event)
+    event.dataTransfer.setData("text/html", event.target);
   });
+
+  el.addEventListener('dragenter', event => {
+    console.log('dragenter')
+  });
+
+  el.addEventListener('dragleave', event => {
+    console.log('dragleave')
+  });
+
+  el.addEventListener('dragend', event => {
+    console.log('dragend')
+    const data = event.dataTransfer.getData("text");
+    event.target.appendChild(document.getElementById(data));
+  });
+
+  el.addEventListener('drop', event => {
+
+    event.preventDefault();
+  if (event.target.className === "empty") {
+    //dragged.parentNode.removeChild(dragged);
+    event.target.appendChild(dragged);
+  }
+  })
 }
