26cdc19155b54c1153a6aeca9c878e9d15d00213
Anna Melnyk <blairw73@gmail.com>
2024-07-18 15:34:47 +1000

    Add

--- drag-n-drop/script.js
+++ drag-n-drop/script.js
@@ -1,53 +1,65 @@
 "use strict"
 
+const CLASS_EMPTY = 'empty';
+const CLASS_BLOCK = 'block';
+const CLASS_DRAGGED = 'dragged';
+const CLASS_DRAGOVER = 'dragover';
+
 const main = document.querySelector(".main")
 let emptyDivs
+let draggableBlock
 
 createDraggableBlocks()
 
 function createDraggableBlocks() {
   for (let i = 0; i < 5; i++) {
     const empty = document.createElement("div")
-    empty.classList.add('empty')
+    empty.classList.add(CLASS_EMPTY)
     main.appendChild(empty)
     addDragListeners(empty)
   }
 
-  emptyDivs = document.querySelectorAll('.empty')
-  const block = document.createElement("div")
-  block.classList.add('block')
-  block.setAttribute('draggable', true)
-  emptyDivs[0].appendChild(block)
+  emptyDivs = document.querySelectorAll(".empty")
+  draggableBlock = document.createElement("div")
+  draggableBlock.classList.add(CLASS_BLOCK)
+  draggableBlock.setAttribute("draggable", true)
+  emptyDivs[0].appendChild(draggableBlock)
 }
 
-
 function addDragListeners(el) {
-  el.addEventListener("dragstart", event => {
-    console.log('dragstart')
-    console.log(event)
-    event.dataTransfer.setData("text/html", event.target);
-  });
+  el.addEventListener("dragstart", (event) => {
+    console.log("dragstart")
+    //event.dataTransfer.setData("text/html", event.target)
+    draggableBlock.classList.add(CLASS_DRAGGED)
+  })
 
-  el.addEventListener('dragenter', event => {
-    console.log('dragenter')
-  });
+  el.addEventListener("dragenter", (event) => {
+    console.log("dragenter")
+    //draggableBlock.classList.add(CLASS_DRAGGED)
+  })
 
-  el.addEventListener('dragleave', event => {
-    console.log('dragleave')
-  });
+  el.addEventListener("dragleave", (event) => {
+    console.log("dragleave")
+    event.target.classList.remove(CLASS_DRAGOVER)
+  })
 
-  el.addEventListener('dragend', event => {
-    console.log('dragend')
-    const data = event.dataTransfer.getData("text");
-    event.target.appendChild(document.getElementById(data));
-  });
+  el.addEventListener("dragover", (event) => {
+    console.log("dragover")
+    event.preventDefault()
+    event.target.classList.add(CLASS_DRAGOVER)
+  })
 
-  el.addEventListener('drop', event => {
+  el.addEventListener("dragend", (event) => {
+    console.log("dragend")
+    draggableBlock.classList.remove(CLASS_DRAGGED)
+    Array.from(emptyDivs).forEach(div => div.classList.remove(CLASS_DRAGOVER))
+  })
 
-    event.preventDefault();
-  if (event.target.className === "empty") {
-    //dragged.parentNode.removeChild(dragged);
-    event.target.appendChild(dragged);
-  }
+  el.addEventListener("drop", (event) => {
+    console.log("drop")
+    if ([...event.target.classList].includes(CLASS_EMPTY)) {
+      draggableBlock.parentNode.removeChild(draggableBlock)
+      el.appendChild(draggableBlock)
+    }
   })
 }
